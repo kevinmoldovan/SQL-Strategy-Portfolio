6443bf9b6713fec5fc380e249eeac30aca8ec67c
kevinmoldovan <59153469+kevinmoldovan@users.noreply.github.com>
2026-06-28 00:16:59 -0400

    Add SQL query for left join on marketing data

---
+++ Project-03-Relational-LTV-Engine/02_left_join_safeguard.sql
@@ -0,0 +1,8 @@
+SELECT 
+  marketing_attribution.customer_id, 
+  marketing_attribution.utm_source, 
+  marketing_attribution.acquisition_cost, 
+ user_revenue.lifetime_spend
+FROM marketing_attribution
+LEFT JOIN user_revenue
+ON marketing_attribution.customer_id = user_revenue.customer_id;
